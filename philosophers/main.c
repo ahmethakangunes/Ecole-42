7dcec5c8a4ce092db1bf75eb778bd8ccf4b2df7e
Ahmet Hakan <agunes@192.168.1.104>
2022-06-28 22:49:29 +0300

    push

--- philosophers/main.c
+++ philosophers/main.c
@@ -38,12 +38,11 @@ int	setup(int argc, char **argv)
 		philo->phi = ft_atoi(argv[1]);
 		while (i < philo->phi)
 		{
-			philo[i].phi = ft_atoi(argv[1]);
 			philo[i].id = i + 1;
-			philo[i].die = 0;
+			philo[i].phi = ft_atoi(argv[1]);
+			philo[i].die = ft_atoi(argv[2]);
 			philo[i].eat = ft_atoi(argv[3]);
 			philo[i].sleep = ft_atoi(argv[4]);
-			philo[i].argcount = argc;
 			if (argc == 6)
 				philo[i].meat = ft_atoi(argv[5]);
 			i++;
---
+++ philosophers/push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ..
+git add .
+git commit -m "push"
+git push
